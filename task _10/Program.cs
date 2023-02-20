bfbda3668291f91637bfd52f41ad0421f3a4fc70
Igor <denlandpud@gmail.com>
2023-02-20 19:17:53 +0300

    task_10

--- task _10/Program.cs
+++ task _10/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+Console.Clear();
+Console.WriteLine("enter a three-digit number");;
+int numb = int.Parse(Console.ReadLine());
+if (numb < 100 || numb > 999)
+{
+    Console.WriteLine("It's not a three-digit number");
+}
+else
+{
+    int result =  (numb/10) % 10;
+    Console.WriteLine($"{numb} = {result}");
+}
